096c8c4ee33bcd52263ec2c84121b71475ca7995
freddwr <freddfrfr@gmail.com>
2018-11-22 20:33:38 +0100

    modificacion necesaria para emprezar

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static(__dirname + './dist/exam1'));
+
+app.get('/*', (req.res)=>{
+	res.sendFIle(path.join(__dirname, '/dist/exam1/index.html'));
+});
+
+
+app.listen(process.env.PORT || 8080, ()=>{
+	console.log('servidor iniciado');
+});
