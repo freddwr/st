6c0055bf96a945d059efcd3b6bdfe73ca3dde81a
Raquelms <medsos.raquel@gmail.com>
2019-02-03 20:36:22 -0400

    inicio fotter y navbar - sin eleccion de color- con errores aun

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { InicioComponent } from './component/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
